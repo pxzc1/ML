ca80c8d80902ed11806d959eb46f8d8d695d56a8
pxzc1 <tonaok2555@gmail.com>
2026-02-25 22:54:41 +0700

    update to latest NixOS stable (branch 25.11)

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Python 3.12 + CUDA";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
   };
 
   outputs = { self, nixpkgs }:
